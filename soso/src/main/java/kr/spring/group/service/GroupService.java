0bafcfb5775871b5dc3a8cad435c2ed347d128cf
ssy2253 <88231613+ssy2253@users.noreply.github.com>
2023-02-20 14:09:58 +0900

    스터디그룹 가입자 상세페이지 컨트롤러, dao 작성(상세페이지 메인 조회기능 추가-일정,공지,멤버)

--- soso/src/main/java/kr/spring/group/service/GroupService.java
+++ soso/src/main/java/kr/spring/group/service/GroupService.java
@@ -1,5 +1,21 @@
 package kr.spring.group.service;
 
-public class GroupService {
+import java.util.List;
 
+import kr.spring.group.vo.GroupCalendarVO;
+import kr.spring.group.vo.GroupNoticeVO;
+import kr.spring.member.vo.MemberVO;
+
+
+public interface GroupService {
+	
+	/*------------메인페이지-----------*/
+	//그룹 규칙 조회
+	public GroupNoticeVO selectRule(int stc_num);
+	//오늘 일정 조회
+	public List<GroupCalendarVO> selectTodayList(int stc_num);
+	//그룹 멤버 조회
+	public int selectCountMember(int stc_num);
+	//그룹 멤버 조회
+	public List<MemberVO> selectMemberList(int stc_num);
 }
--- soso/src/main/java/kr/spring/group/service/GroupServiceImpl.java
+++ soso/src/main/java/kr/spring/group/service/GroupServiceImpl.java
@@ -1,5 +1,42 @@
 package kr.spring.group.service;
 
-public class GroupServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.spring.group.dao.GroupMapper;
+import kr.spring.group.vo.GroupCalendarVO;
+import kr.spring.group.vo.GroupNoticeVO;
+import kr.spring.member.vo.MemberVO;
+
+
+@Service
+@Transactional
+public class GroupServiceImpl implements GroupService{
+	
+	@Autowired
+	private GroupMapper groupMapper;
+	
+	@Override
+	public GroupNoticeVO selectRule(int stc_num) {
+		return groupMapper.selectRule(stc_num);
+	}
+	@Override
+	public List<GroupCalendarVO> selectTodayList(int stc_num){
+		return groupMapper.selectTodayList(stc_num);
+	}
+	@Override
+	public int selectCountMember(int stc_num) {
+		return groupMapper.selectCountMember(stc_num);
+	} 
+	
+	@Override
+	public List<MemberVO> selectMemberList(int stc_num) {
+		return groupMapper.selectMemberList(stc_num);
+	} 
+	
+	
 
 }
